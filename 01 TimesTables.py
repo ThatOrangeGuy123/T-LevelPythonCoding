e55d0c21516fb2e35caa8841df9491948222682f
ThatOrangeGuy123 <itsolivergrant@gmail.com>
2024-10-17 11:58:28 +0100

    Updated to add function

--- 01 TimesTables.py
+++ 01 TimesTables.py
@@ -1,9 +1,12 @@
+#Define Function
+def timestables(NumOut):
+    #Enter Loop of 5 increments
+    for i in range(0,5):
+        #Double the Number
+        NumOut = int(NumOut) * 2
+        #Output the result
+        print(NumOut)
 #User Inputs Initial Number
 UserNumber = input("Enter a Number: ")
-NumOut = UserNumber
-#Enter Loop of 5 increments
-for i in range(0,5):
-    #Double the Number
-    NumOut = int(NumOut) * 2
-    #Output the result
-    print(NumOut)
+#Call Function
+timestables(UserNumber)
